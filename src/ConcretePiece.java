f0d0179abb532b3cb92bfcc02a1b1f199639965e
GuyShimonN <guyguy845@gmail.com>
2024-01-09 22:00:08 +0200

    gf

---
+++ src/ConcretePiece.java
@@ -0,0 +1,31 @@
+public class ConcretePiece implements Piece{
+    private final ConcretePlayer owner;
+    private String name;
+    private int num_step;
+    public ConcretePiece(ConcretePlayer p,String name){
+        this.owner = p;
+        this.name= name;
+        this.num_step=0;
+    }
+    @Override
+    public Player getOwner() {
+        return owner;
+    }
+
+    public String getName(){
+        return name;
+    }
+    @Override
+    public String getType() {
+//        String t = getName();
+//        String s = t.charAt(0) + "";
+//        if (s.equals("k")) {
+//            return "King";
+//        }
+//        return "Pawn";
+  return "";
+    }
+    public void inc_step(){
+        num_step++;
+    }
+}
---
+++ src/Pawn.java
@@ -0,0 +1,20 @@
+public class Pawn extends ConcretePiece{
+
+    private int kill;
+    public Pawn(ConcretePlayer p,String name) {
+        super(p,name);
+        this.kill=0;
+    }
+    @Override
+    public String getType() {
+        return "♙";
+    }
+
+
+    public int getKill() {
+        return kill;
+    }
+    public void inc_kill(){
+        this.kill++;
+    }
+}
